12ba22a11cdc65f2acfaae906f8aa71776bf1c83
Zoltan KADA <kada.zoli@gmail.com>
2025-12-22 16:47:33 +0100

    Bump version to 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "hu.kadatsoft.smsreply"
         minSdk = 29
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     buildTypes {
