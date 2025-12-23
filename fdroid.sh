055b2fa3a036bc0d0235ee62bbf9c9a5c6099e21
Zoltan KADA <kada.zoli@gmail.com>
2025-12-23 14:03:56 +0100

    Add Gradle wrapper for F-Droid

---
+++ fdroid.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+gradle wrapper
+
+git clone https://gitlab.com/kada.zoli/fdroiddata.git
+cd fdroiddata
+
+printf "sdk_path: /opt/android-sdk\nserverwebroot: /tmp/fdroid\n" > config.yml
+chmod 600 config.yml
+mkdir -p /tmp/fdroid
+
+yay -S python311
+
+python3.11 -m venv fdroid-env
+source fdroid-env/bin/activate
+
+pip install --upgrade pip
+pip install fdroidserver
+
+export PATH=/home/zolti/Android/Sdk/build-tools/34.0.0:$PATH
+
+fdroid readmeta
+fdroid import --url https://github.com/kzolti/sms-reply --subdir app
+fdroid checkupdates hu.kadatsoft.smsreply
+fdroid rewritemeta hu.kadatsoft.smsreply
+fdroid lint hu.kadatsoft.smsreply
+fdroid build -v -l hu.kadatsoft.smsreply
